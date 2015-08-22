1306e80ad9a491061719e7337a46194813d69ccb
Jon Harlan <jonharlan@JONATHANs-MacBook-Pro-2.local>
2015-08-22 16:49:10 -0700

    node + ruby + digits

---
+++ config/application.rb
@@ -0,0 +1,32 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module LC
+  class Application < Rails::Application
+
+
+
+    # via https://gist.github.com/afeld/5704079
+
+    # We don't want the default of everything that isn't js or css, because it pulls too many things in
+    config.assets.precompile.shift
+
+    # Explicitly register the extensions we are interested in compiling
+    config.assets.precompile.push(Proc.new do |path|
+      File.extname(path).in? [
+        '.html', '.erb', '.haml',                 # Templates
+        '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
+        '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+      ]
+    end)
+
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
